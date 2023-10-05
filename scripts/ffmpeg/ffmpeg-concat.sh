00089c2e91a6af4eba4381f84e953480595d819d
jhonesto <jp.honesto@gmail.com>
2023-10-05 04:23:39 -0300

    added script for ffmpeg

---
+++ scripts/ffmpeg/ffmpeg-concat.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#Definindo as variáveis
+BASE="$PWD/concat"
+LIST="filelist.txt"
+
+for f in $BASE/*; do echo "file '$f'" >> $LIST; done
+
+#Concatenando os arquivos de acordo como quantidade passada 
+ffmpeg -f concat -safe 0 -i $LIST -c copy $BASE/output.mp4
+
+rm $LIST 
