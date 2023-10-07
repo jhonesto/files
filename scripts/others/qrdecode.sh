64574ac44ab0e75021c1a4b150b741f035bceb91
jhonesto <jp.honesto@gmail.com>
2023-10-07 15:18:02 -0300

    add qrcode script

---
+++ scripts/others/qrdecode.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Script para converter o QR code em texto
+
+# Verifique se o programa qrencode está instalado
+command -v qrencode >/dev/null 2>&1 || { echo >&2 "Esse script requer o programa qrencode.  Por favor, instale-o primeiro antes de continuar."; exit 1; }
+
+# Solicitar ao usuário a inserção do código QR
+echo Por favor, insira o código QR
+read qrcode
+
+# Codificar o código QR para texto
+qrencode -t UTF8 "$qrcode"
