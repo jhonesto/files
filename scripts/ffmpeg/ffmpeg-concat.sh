48539e376ec151e93a54f016afc1b33b21e51b20
Joao Paulo <jp.honesto@gmail.com>
2023-10-27 23:36:27 -0300

    teste alpine linux

--- scripts/ffmpeg/ffmpeg-concat.sh
+++ scripts/ffmpeg/ffmpeg-concat.sh
@@ -4,7 +4,7 @@
 BASE="$PWD/concat"
 LIST="filelist.txt"
 
-for f in $BASE/*; do echo "file '$f'" >> $LIST; done
+for f in $BASE/*; do echo "file '$f'" >> $LIST; done 
 
 #Concatenando os arquivos de acordo como quantidade passada 
 ffmpeg -f concat -safe 0 -i $LIST -c copy $BASE/output.mp4
