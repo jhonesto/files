d2a4930281395b9fdab15f3ce058dd5a1de04c6a
Joao Paulo <jp.honesto@gmail.com>
2023-11-05 21:17:50 -0300

    alterando a mensagem de input

--- scripts/others/qrdecode.sh
+++ scripts/others/qrdecode.sh
@@ -5,7 +5,7 @@
 command -v qrencode >/dev/null 2>&1 || { echo >&2 "Esse script requer o programa qrencode.  Por favor, instale-o primeiro antes de continuar."; exit 1; }
 
 # Solicitar ao usuário a inserção do código QR
-echo Por favor, insira o código QR
+echo Por favor, insira o texto para gerar um QR Code.
 read qrcode
 
 # Codificar o código QR para texto
