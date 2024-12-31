950203aa2540fad9d0b56945a8ba3aa46613bfd3
Joao Paulo <jp.honesto@gmail.com>
2024-12-31 02:57:58 -0300

    Programa que resolve algoritimo FizzBuzz em C Bricandeira com o uso extensivo da diretiva #define para substituição de caracteres antes da compilação

---
+++ programas/fizzbuzz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#define o
+#define oo
+#define oooo
+#define oooooooooooo
+#define ooooooooooooooooo {
+#define ooooooooooooooooooooooooooooooooooo 
+#define ooooooooooooooooooooooooooooooooooooooooo }
+
+oooooooooooo int main() ooooooooooooooooo
+o                                       o
+o  oooo for (int i= 1; i<=100; i++) oo  o 
+o  o                                 o  o
+o  o      i%15? i%3? i%5?    o       o  o
+o  o      printf("%d ", i)   :       o  o
+o  o      printf("Buzz ")    :       o  o
+o  o      printf("Fizz ")    :       o  o
+o  o      printf("FizzBuzz ");       o  o
+o  o                                 o  o              
+o  ooooooooooooooooooooooooooooooooooo  o
+o                                       o
+ooooooooooooooooooooooooooooooooooooooooo
